58ebb17652197d9af5e284ef2f2ddd685157505b
Dylan Williams <dylanawilliams213@gmail.com>
2023-11-07 19:30:14 -0600

    First Commit

---
+++ Unit_103/UnderstandingVariables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#script ops 201 class Ops Challenge Solutions
+#Author: Dylan Williams
+#Nov 7 2023
+#purpose: Understand Variables
+
+Variables=$name!
+echo "What is your name?"
+read name
+sleep 3
+echo "Welcome to the class $name"+"!"
