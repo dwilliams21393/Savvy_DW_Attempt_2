472988200b8b5fa3ce179970e1e00ca2d454df4e
Dylan Williams <dylanawilliams213@gmail.com>
2023-11-08 19:46:14 -0600

    First Commit

---
+++ Unit_103/ElseIfLoop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#script ops 201 class Ops Challenge Solutions
+#Author: Dylan Williams
+#Nov 7 2023
+#purpose: Understanding Condoitional Statement Else If
+
+#Variables
+Variables=$Number
+
+echo "Please enter a number."
+read Number
+sleep 3
+echo "Your number is $Number"
+sleep 3
+ 
+if (( $Number > 5 ))
+    then 
+        echo "Your number is greater than 5."
+elif (( $Number < 5 ))
+    then 
+        echo "Your number is less than 5."
+else echo "Your number is equal to 5."
+fi
+sleep 3
+echo "Thank you for running my program. Good Bye!"
+
